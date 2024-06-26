c263b528fa557983f7a25fd942f9809a172582e5
Dany <dany-de-carvalho@outlook.fr>
2024-06-26 13:30:55 +0200

    card display

--- src/app/api/party/route.ts
+++ src/app/api/party/route.ts
@@ -2,6 +2,6 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 export async function GET() {
-  const data = await prisma.user.findMany();
+  const data = await prisma.party.findMany();
   return Response.json(data);
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,9 @@
+import { CardParty } from "@/components/component/card-party";
+
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24"></main>
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <CardParty />
+    </main>
   );
 }
